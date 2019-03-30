98bdbf89d8d097880b8115be81a2086c4be8b7a1
pythonCat <pythoncat@qq.com>
2019-03-30 09:56:09 +0800

    2.12.3

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+// 编写一个程序，把年龄换成天数，不考虑闰年
 int main() {
-    printf("Victor\n");
-    printf("China");
+    int age = 29;
+    int days = age*365;
+    printf("age %d equals %d days",age,days);
     return 0;
 }
