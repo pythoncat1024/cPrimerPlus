0e56c47ab68c0a8a92554b7a312f4d5d47647d68
pythonCat <pythoncat@qq.com>
2019-03-30 16:30:15 +0800

    3.11.2

--- main.c
+++ main.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-#include <limits.h>
-#include <float.h>
 
-// 观察系统处理整数，浮点数的上溢，下溢
+// 要求输入一个ASCII码值，然后打印输入的字符。
 int main(void){
-    int i_max = INT_MAX;
-    printf("max int : %d , max int plus:%d \n", i_max,i_max+1);
-    float f_max = FLT_MAX;
-    printf("max float = %f\n# max float puls = %f\n",f_max,f_max+1);
-    printf("f_max = %e ### %a \n",f_max,f_max);
-    int i_min = INT_MIN;
-    float f_min = FLT_MIN;
-    printf("i_min=%d,i_min_-1=%d\n",i_min,i_min-1);
-    printf("f_min=%.5f,f_min-1=%.5f\n",f_min,f_min-1);
-    // --- 格式化输出---
-    int age = 26;
-    printf("age = %d , %c , %u , %#o , %#x\n",age,age,age,age,age);
+    int input;
+    printf("输入一个ASCII码值(应该是0-127之间？)：");
+    scanf("%d",&input);
+    printf("num = %d , also equal %c ---END\n",input,input);
     return 0;
 }
