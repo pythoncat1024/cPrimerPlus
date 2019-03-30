d0d585743d2af4995a0a0503b45eaeeebf5d9040
pythonCat <pythoncat@qq.com>
2019-03-30 10:22:17 +0800

    2.12.8

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-void smile(void);
+void one_three(void);
+void two(void);
 
 int main(void){
-    smile();
-    smile();
-    smile();
-    printf("\n");
-    smile();
-    smile();
-    printf("\n");
-    smile();
-    printf("\n");
+    printf("starting now:\n");
+    one_three();
+    printf("done!\n");
     return 0;
 }
 
-void smile(void){
-    printf("Smile!");
+void one_three(void){
+    printf("one\n");
+    two();
+    printf("three\n");
+}
+
+void two(void){
+    printf("two\n");
 }
