fba38b6c17171bb77ce2e056b2d6ee2add635d23
pythonCat <pythoncat@qq.com>
2019-03-30 16:34:00 +0800

    3.11.3

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-// 要求输入一个ASCII码值，然后打印输入的字符。
 int main(void){
-    int input;
-    printf("输入一个ASCII码值(应该是0-127之间？)：");
-    scanf("%d",&input);
-    printf("num = %d , also equal %c ---END\n",input,input);
+    printf("\a");
+    printf("Startled by the sudden sound, Sally shouted,\n"
+            "\"By the Great Pumpkin, what was that!\"\n");
     return 0;
 }
