49b27477c46d90bdb7dfc68b8808b46e894c2cc5
pythonCat <pythoncat@qq.com>
2019-03-30 23:26:29 +0800

    3.11.6

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+// 一个水分子的质量大约是 3.156e-23克，1夸脱水大约是950克。输入夸脱数，输出水分子数量
 int main(void){
-    long double one_year = 3.156e7; // 一年有多少秒
-    int age;
-    printf("请输入你的年龄：");
-    scanf("%d",&age);
-    long double total_seconds = age*one_year;
-    printf("%d age == %.2Lf secnonds ==== %Le === %La\n",age,total_seconds,total_seconds,total_seconds);
+    float weight = 3.156e-23F;
+    float pre_num = 1.0F/weight; // 1克水含有多少个水分子
+    float input;
+    printf("输入水的数量：（单位夸脱）");
+    scanf("%f",&input);
+    float total = pre_num * input;
+    printf("%.02f 夸脱的水中含有水分子的数量为：%.03e\n",input,total);
     return 0;
 }
