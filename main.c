e7e02569d0c6da68c0361829b77c15a0ae4c6afe
pythonCat <pythoncat@qq.com>
2019-04-07 22:43:08 +0800

    4.8.4

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 int main(void){
-    printf("input a float number:");
-    float num;
-    scanf("%g",&num);
-    printf("number == %.1f or %.1e\n", num,num);
-    printf("number == %+.3f or %E\n", num,num);
+    printf("input your height (cm) and name:");
+    float height;
+    char name[30];
+    scanf("%g",&height);
+    scanf("%s",name);
+    printf("%s,you are %g m tall\n", name,height/100);
     return 0;
 }
