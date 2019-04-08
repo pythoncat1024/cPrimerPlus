ad126595d645d8585ed1ba11ef3f3fc421d1427c
pythonCat <pythoncat@qq.com>
2019-04-08 22:00:59 +0800

    4.8.8

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <float.h>
 
+#define GAL 3.875
+#define MI 1.609
 int main(void){
-    double d = 1.0/3.0;
-    float f = 1.0f/3.0f;
-    const float ff = FLT_DIG;
-    const double dd = DBL_DIG;
-    printf(".6: %.6f , %.6f\n" , f , d);
-    printf(".12: %.12f , %.12f\n" , f , d);
-    printf(".16: %.16f , %.16f\n" , f , d);
-    printf("const: %g , %G\n", ff , dd);
+    printf("输入旅行的里程数（mi）和消耗的汽油量（us gal）:");
+    float distance;
+    float total_gal;
+    scanf("%f %f", &distance , &total_gal);
+    float per = distance / total_gal;
+    float distance_km = distance * MI;
+    float total_l = GAL * total_gal;
+    float per2 = total_l/ (100 * distance_km);
+
+    printf("行驶 %.1f 英里，消耗 %.1f 加仑的汽油，"
+           "平均消耗为 %.1f(mi/gal)\n" ,
+            distance , total_gal , per);
+
+    printf("相当于行驶了 %.1f 千米，消耗 %.1f 升的汽油，"
+            "平均消耗为 %1f (l%%km)\n" ,
+            distance_km , total_l , per2);
     return 0;
 }
