666f31322caef50677c06ac86aaf4d6b34d558b1
pythonCat <pythoncat@qq.com>
2019-04-08 21:44:48 +0800

    4.8.7

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <float.h>
 
 int main(void){
-    char name[20];
-    char ln[20];
-    printf("input your first name:");
-    scanf("%s" , name);
-    printf("input your last name:");
-    scanf("%s" , ln);
-
-    int len_name = strlen(name);
-    int len_ln = strlen(ln);
-    printf("%-20s %-20s\n", name , ln);
-    printf("%-20d %-20d\n" , len_name , len_ln);
+    double d = 1.0/3.0;
+    float f = 1.0f/3.0f;
+    const float ff = FLT_DIG;
+    const double dd = DBL_DIG;
+    printf(".6: %.6f , %.6f\n" , f , d);
+    printf(".12: %.12f , %.12f\n" , f , d);
+    printf(".16: %.16f , %.16f\n" , f , d);
+    printf("const: %g , %G\n", ff , dd);
     return 0;
 }
