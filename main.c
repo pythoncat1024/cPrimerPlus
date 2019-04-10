ce9c845069d387ca52bcb6d23fe1168d94c25a3a
pythonCat <pythoncat@qq.com>
2019-04-10 21:23:55 +0800

    5.11.1

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#define G 'g'
+#define HOUR 60
 
 int main(void) {
+    int mins;
+    do {
+        printf("输入分钟数( <=0 quit ): ");
+        scanf("%d" , &mins);
+        if (mins <=0 ) {
+            break;
+        }
+        int hour = mins / HOUR;
+        int min = mins % HOUR;
+        if (hour == 0) {
+            printf("spent: %d mins.\n" , min);
+        } else {
+            printf("spent: %d hours and %d mins.\n" , hour , min);
+        }
+    } while (mins > 0);
 
-    char ch = 'a' -1 ;
-    while( ch++ < G ) {
-        printf("%5c" , ch);
-    }
-    printf("\n");
+    printf("end.\n");
     return 0;
 }
