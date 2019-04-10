39426747923391d23525fb22dc2dfcf0d7ee7e1b
pythonCat <pythoncat@qq.com>
2019-04-10 21:54:54 +0800

    5.11.3

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
+#define WEEK 7
 int main(void) {
-    int num;
-    printf("请随便输入一个整数，我将打印[n,n+10]：");
-    scanf("%d" , &num);
-    const int max = num + 10;
-    while (num <= max) {
-        printf("%d\t", num);
-        num++;
+    printf("天数转周数+天数了，请输入天数：");
+    int days;
+    scanf("%d" , &days);
+    while (days > 0) {
+
+        int w = days / WEEK;
+        int d = days % WEEK;
+        printf("%d days are %d weeks, %d days.\n" , days , w , d);
+        printf("天数转周数+天数了，请输入天数：");
+        scanf("%d" , &days);
     }
-    num--;
-    printf("\n");
+    printf("\n ### 输入不合法。###\n");
     return 0;
 }
