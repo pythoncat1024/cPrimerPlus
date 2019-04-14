a44a935bd751536e198a4533b8deed79e2fbc6f3
pythonCat <pythoncat@qq.com>
2019-04-14 16:47:13 +0800

    6.16.10

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-float compute(float arg1 , float arg2);
-
 int main(void)
 {
-    printf("请输入两个浮点数, 间隔一个空格: ");
-    float a , b;
-    float r;
-    while ( 2 == scanf("%f %f" , &a , &b) )
+    int lower , upper;
+    printf("Enter lower and upper integer limits: ");
+    while ( 2 == scanf("%d %d" , &lower , &upper)
+            && lower < upper)
     {
-        r = compute( a , b );
-        printf("result of [(%.2f - %.2f) / (%.2f * %.2f)] = %.2f\n" ,
-                a , b , a ,b ,r);
-    printf("请输入两个浮点数, 间隔一个空格: ");
+        long int x = lower * lower;
+        long int y = upper * upper;
+        long int z = 0;
+        long int fin;
+        for(int i = lower+1 ; i< upper ; i++ )
+        {
+            z += i * i;
+        }
+        fin = x + y + z;
+        printf("The sum of the squares from %ld to %ld is %ld\n" ,
+                x , y , fin);
+        printf("Enter lower and upper integer limits: ");
     }
-    printf("非法输入 Done. \n");
+    printf("Done\n");
     return 0;
-}
-
-float compute(float a , float b)
-{
-    return (a-b) / (a*b);
 }
