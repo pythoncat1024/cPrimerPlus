a303c748b586301981cda8a26d36e92eb614216b
pythonCat <pythoncat@qq.com>
2019-04-14 08:47:04 +0800

    5.11.9

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+void Temperatures(double);
+
 int main(void) {
-    printf("This program computes moduli.\n");
-    printf("Enter an integer to serve as the second operand (<= 0 to quit): ");
-    int op1 , op2 , result;
-    scanf("%d" , &op2);
-    printf("Now enter the first operand: ");
-    scanf("%d" , &op1);
-    while( op1 > 0 ) {
-        result = op1 % op2;
-        printf("%d %% %d is %d\n" , op1 , op2 , result);
-        printf("Enter next number for first operand (<= 0 to quit): ");
-        scanf("%d" , &op1);
+    printf("输入一个华氏温度值(非数字退出程序):");
+    double hua;
+    int sc;
+    while ( (sc = scanf("%lf" , &hua)) ==1) {
+        Temperatures(hua);
     }
-    printf("Done\n");
+    printf("输入不合法 Done.\n");
     return 0;
+}
+
+void Temperatures(double hua) {
+    double she,kai;
+    const double factor = 5.0 / 9.0;
+    const double more = 32;
+    const double absolute = 273.16;
+    she = factor * (hua - more);
+    kai = she + absolute;
+    printf("%5.2f 华氏度相当于%5.2f 摄氏度，相当于%5.2f 开氏度.\n \n" ,
+            hua , she , kai);
+    printf("输入一个华氏温度值(非数字退出程序):");
 }
