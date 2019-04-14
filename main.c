28eddec98b22caeae9663c524750be4c13dac169
pythonCat <pythoncat@qq.com>
2019-04-14 16:39:32 +0800

    6.16.9

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+float compute(float arg1 , float arg2);
+
 int main(void)
 {
     printf("请输入两个浮点数, 间隔一个空格: ");
@@ -7,11 +9,16 @@ int main(void)
     float r;
     while ( 2 == scanf("%f %f" , &a , &b) )
     {
-        r = (a-b) / (a*b);
+        r = compute( a , b );
         printf("result of [(%.2f - %.2f) / (%.2f * %.2f)] = %.2f\n" ,
                 a , b , a ,b ,r);
     printf("请输入两个浮点数, 间隔一个空格: ");
     }
     printf("非法输入 Done. \n");
     return 0;
+}
+
+float compute(float a , float b)
+{
+    return (a-b) / (a*b);
 }
