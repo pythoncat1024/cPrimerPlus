8eb076cf0fbb4712326d5e6842b46c4c14cfaa9a
pythonCat <pythoncat@qq.com>
2019-04-14 14:52:32 +0800

    6.16.2

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#define LEN 5
+#define CHAR '$'
 int main(void)
 {
-    char abc[] = "abcdefghijklmnopqrstuvwxyz";
-    for( int index = 0 ; index < 26 ; index++ )
-        printf("%c " , abc[index]);
+    for( char i = 0 ; i < LEN ; i++ )
+    {
+        for( char j = 0; j <= i ; j++ )
+        {
+            printf("%c" , CHAR);
+        }
+        printf("\n");
+    }
     return 0;
 }
