7807c9e8f283ad3c4cf88dd122b3aa33025e1d30
pythonCat <pythoncat@qq.com>
2019-04-15 21:50:03 +0800

    6.16.15

--- main.c
+++ main.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#define SIZE 8
-
-double sum_arr(int index);
-
+#define LENGTH 255
 int main(void)
 {
-    double arr1[SIZE];
-    double arr2[SIZE];
-    double tmp;
-    printf("请依次输入%d个数.\n", SIZE);
-    for( int i = 0; i < SIZE; i++ )
+    printf("请输入一行文字:");
+    char str[LENGTH];
+    int pos = 0;
+    int read;
+    while( pos < LENGTH
+            && ( read = scanf("%c" , &str[pos]) ) == 1
+            && str[pos] != '\n')
     {
-        printf("请输入第 %d 个数: ", i+1 );
-        scanf("%lf" , &arr1[i]);
+        pos++;
+    }
+    printf(" pos == %d\n" , pos);
+    if( pos == LENGTH )
+        str[pos-1] = '\0';
+    else
+        str[pos] = '\0';
 
-        tmp = 0;
-        for(int k  = 0; k <= i; k++ ){
-            tmp += arr1[k];
-        }
-        arr2[i] = tmp;
-    }
-    for(int i = 0; i < SIZE; i++)
-    {
-        printf("%8.2f", arr1[i]);
-    }
-    printf("\n");
-    for(int i = 0; i < SIZE; i++)
-    {
-        printf("%8.2f" , arr2[i]);
-    }
-    printf("\n");
+    printf("%s\n", str);
     return 0;
 }
