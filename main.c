f59ae369e720a04d4051828fe5483db5b761e607
pythonCat <pythoncat@qq.com>
2019-04-18 21:36:01 +0800

    while 表达式中逗号的妙用

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void)
     printf("请输入任意一串字符:");
     char ch;
     // ( ch = getchar ) != -1 ; 加括号的原因是赋值运算符优先级最低，低于比较运算符
-    while( ( ch = getchar() ) != '\n' )
+    while( ch = getchar(), ch != '\n' )
     {
         if( isalpha(ch) )
         {
