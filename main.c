d2e711fc0e82615c770b4ead2a814a4f625292e5
pythonCat <pythoncat@qq.com>
2019-04-18 22:21:48 +0800

    统计行数，单词数，字符数

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <ctype.h>
-int main(void)
-{
-    printf("请输入任意一串字符:");
-    char ch;
-    // ( ch = getchar ) != -1 ; 加括号的原因是赋值运算符优先级最低，低于比较运算符
-    while( ch = getchar(), ch != '\n' )
-    {
+#include <stdbool.h>
+#define STOP '|'
+#define NEXT '\n'
+
+int main(void) {
+    bool in_word = false; // 开始一个单词
+    int n_chars, n_lines, n_words;
+    char ch, prev;
+    n_chars = n_lines = n_words = 0;
+    prev = '\n';
+    printf("统计行数，字符数，单词数( | to quit ):\n");
+
+    while( (ch = getchar()) != STOP ) {
         if( isalpha(ch) )
-        {
-             putchar( ch+1 );
+            n_chars++;
+
+        if( !in_word && !isspace(ch) ) {
+            in_word = true;
+            n_words++;
         }
-        else
-        {
-            putchar( ch );
+        else if( in_word && isspace(ch) ) {
+            in_word = false;
         }
+        if( ch == NEXT ) {
+            n_lines++;
+        }
+        prev = ch;
+        //printf("--- total lines = %d , total words = %d , total chars = %d\n",
+        //    n_lines, n_words, n_chars);
     }
-    putchar( ch );
+
+    if( prev != NEXT ) {
+        // last char == '|'
+        n_lines++;
+        n_words++;
+    }
+
+    // printf("prev = %c\n",prev);
+
+    printf("total lines = %d , total words = %d , total chars = %d\n",
+            n_lines, n_words, n_chars);
     return 0;
 
 }
