72800283f67f7caf7b49e078a59a897715f5ef85
pythonCat <pythoncat@qq.com>
2019-04-21 21:46:34 +0800

    7.12.6

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
 #define STOP '#'
-#define POINT '.'
-#define RP '!'
+
 int main(void)
 {
-    printf("替换和统计 . ! 出现的次数:\n");
-    char ch;
-    int replace = 0;
-    while( (ch = getchar()) != STOP )
+    printf("统计输入中的 'ei' 出现的次数，不包含单引号:\n");
+    char ch, prev;
+    prev = ' ';
+    int ei = 0;
+    while( ( ch = getchar() ) != STOP )
     {
-        switch( ch )
-        {
-            case POINT:
-                //printf("%c", RP);
-                replace++;
-                break;
-            case RP:
-                //printf("%c%c", RP, RP);
-                replace++;
-                break;
-            default:break;
+        if( 'e' == prev && 'i' == ch ){
+            ei++;
         }
+        prev = ch;
     }
-    printf("\n");
-    printf("total repalce == %d\n", replace);
+
+    while( getchar() != '\n' )
+        continue;
+
+    printf("count if 'ei' = %d\n", ei);
     return 0;
 }
