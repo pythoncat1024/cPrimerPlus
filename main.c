3fa0c1f528396052b23e706af4ab3464d22f7e0c
pythonCat <pythoncat@qq.com>
2019-04-21 20:49:34 +0800

    7.12.4

--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <ctype.h>
-#define STOP 0
+#define STOP '#'
+#define POINT '.'
+#define RP '!'
 int main(void)
 {
-    printf("请输入整数啊:\n");
-    int num;
-    int n1 = 0;
-    int n2 = 0;
-    int total_n1 = 0;
-    int total_n2 = 0;
-    while( scanf("%d", &num) ==1 && num != 0 )
+    printf("替换和统计 . ! 出现的次数:\n");
+    char ch;
+    int replace = 0;
+    while( (ch = getchar()) != STOP )
     {
-        if( num % 2 == 0 )
+        if( ch == POINT )
         {
-            n2++;
-            total_n2 += num;
+            //printf("%c", RP);
+            replace++;
+        }
+        else if ( ch == RP )
+        {
+            //printf("%c%c", RP, RP);
+            replace++;
         }
         else
         {
-            n1++;
-            total_n1 += num;
+            //putchar(ch);
+            ;
         }
     }
-    while( getchar() !='\n' )
-        continue;
-    printf("n2 = %d , n1 = %d , avg n2 = %.1f , avg n1 = %.1f\n",
-            n2, n1, 1.0f*total_n2/n2, 1.0f*total_n1/n1);
+    printf("\n");
+    printf("total repalce == %d\n", replace);
     return 0;
 }
