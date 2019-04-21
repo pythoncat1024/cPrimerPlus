0609b840fbfb902bb64900bc6912bbaa686a3c0a
pythonCat <pythoncat@qq.com>
2019-04-21 20:38:01 +0800

    7.12.3

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
-#define STOP '#'
+#define STOP 0
 int main(void)
 {
-    printf("随便输入，#号结束:\n");
-    char ch;
-    int pos = 0;;
-    while((ch = getchar()) != STOP )
+    printf("请输入整数啊:\n");
+    int num;
+    int n1 = 0;
+    int n2 = 0;
+    int total_n1 = 0;
+    int total_n2 = 0;
+    while( scanf("%d", &num) ==1 && num != 0 )
     {
-        if (ch == '\n')
-            continue;
-        if(pos%8 == 0)
-            printf("\n");
-        printf("%c,%3d ", ch, ch);
-        pos++;
-
+        if( num % 2 == 0 )
+        {
+            n2++;
+            total_n2 += num;
+        }
+        else
+        {
+            n1++;
+            total_n1 += num;
+        }
     }
+    while( getchar() !='\n' )
+        continue;
+    printf("n2 = %d , n1 = %d , avg n2 = %.1f , avg n1 = %.1f\n",
+            n2, n1, 1.0f*total_n2/n2, 1.0f*total_n1/n1);
     return 0;
 }
