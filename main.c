da3464cc83513bc6df97da03eec65761269e7771
pythonCat <pythoncat@qq.com>
2019-04-21 22:01:07 +0800

    7.12.7

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-#include <ctype.h>
-#define STOP '#'
-
+#define tax3 0.15
+#define tax15 0.20
+#define tax_rest 0.25
+#define pay 1000
 int main(void)
 {
-    printf("统计输入中的 'ei' 出现的次数，不包含单引号:\n");
-    char ch, prev;
-    prev = ' ';
-    int ei = 0;
-    while( ( ch = getchar() ) != STOP )
-    {
-        if( 'e' == prev && 'i' == ch ){
-            ei++;
-        }
-        prev = ch;
+    printf("输入一周工作的小时数:\n");
+    double time;
+    double total = 0;
+    double tax = 0;
+    double rest = 0;
+    scanf("%lf", &time);
+    if( time <= 40 ){
+        total = time * pay;
+    } else {
+        total = 40 * pay + (time - 40) * pay;
     }
+    if( total <= 300 )
+    {
+        tax = tax3 * total;
+    }else if( total < 300 + 150 )
+    {
+        tax = tax3 * 300 + (total - 300) * tax15;
+    } else {
+        tax = tax3 * 300 + 150 * tax15 + (total - 300 -150) * tax_rest;
+    }
+    rest = total - tax;
 
-    while( getchar() != '\n' )
-        continue;
-
-    printf("count if 'ei' = %d\n", ei);
+    printf("%.1lf hour get money total = %.1lf, tax = %.1lf, rest = %.1lf\n",
+            time, total, tax, rest);
     return 0;
 }
