1d18cb339f0aa6bd106a0fa40f6ab277bb2b3aca
pythonCat <pythoncat@qq.com>
2019-04-27 15:37:24 +0800

    读取任何输入，并执行输出

--- main.c
+++ main.c
@@ -1,39 +1,12 @@
 #include <stdio.h>
 
-int isPrime(int num);
 int main(void)
 {
-    printf("请输入正整数 n , <=0 to quit: ");
-    int num;
-    int start = 2;
-    while( 1 == scanf("%d", &num) && num >0 )
+    printf("input anything\n");
+    char ch;
+    while( (ch = getchar()) != EOF )
     {
-        for(int i = start; i <= num; i++)
-        {
-            if(isPrime(i))
-                printf("%d ", i);
-        }
-        printf("\n");
-        printf("下一个正整数? ");
+        putchar(ch);
     }
-    printf("\n");
-    printf("illegal input, quit.\n");
-
     return 0;
-}
-
-int isPrime(int num)
-{
-    int prime = 1;
-    if( num < 2 )
-        return 0; // 不是素数
-    for(int k = 2; k * k <= num; k++)
-    {
-        if( num % k == 0 )
-        {
-            prime = 0;
-            break;
-        }
-    }
-    return prime;
 }
