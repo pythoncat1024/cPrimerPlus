de7d5643ba43eaaafbc08ff25fbd819b0cbce36f
pythonCat <pythoncat@qq.com>
2019-04-27 16:08:27 +0800

    8.11.3

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
     printf("统计输入的字符数:\n");
     char ch;
-    int count = 0;
+    int count_lower = 0;
+    int count_upper = 0;
+    int count_number = 0;
     while( (ch = getchar()) != EOF )
     {
-        count++;
+        if(islower(ch)) {
+            count_lower++;
+        }else if(isupper(ch)) {
+            count_upper++;
+        }else if(isnumber(ch)) {
+            count_number++;
+        }
     }
-    printf("count of input = %d\n", count);
+    printf("count of lower = %d,"
+            "count of upper = %d,"
+            "count of number = %d."
+            "\n", count_lower, count_upper, count_number);
     return 0;
 }
