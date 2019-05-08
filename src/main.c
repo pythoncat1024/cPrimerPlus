e8a3ffae1acfd285f3599cf720f3edcb02065c61
pythonCat <pythoncat@qq.com>
2019-05-08 21:42:28 +0800

    9.11.1

--- src/main.c
+++ src/main.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-// 交换两个变量的值
-void swap(int* a,int* b);
+double min(double x,double y);
 int main(void)
 {
-    int a = 13;
-    int b = 27;
-
-    printf("origin val: a=%d,b=%d\n", a, b);
-    swap(&a, &b);
-    printf("after swap: a=%d,b=%d\n", a, b);
+    double a = 23;
+    double b = 21;
+    printf("min in %g,%g is %g\n", a, b, min(a,b));
     return 0;
 }
-
-void swap(int* x, int* y)
+double min(double xx,double yy)
 {
-    int temp = *x;
-    // 由 *x = *y 也可以看出 解引用对应的数据可以是左值，也可以是右值
-    *x = *y;
-    *y = temp;
+    return xx > yy ? yy : xx;
 }
-
