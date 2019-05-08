0323ba49f9eea7388fb6757a456f5eb3b584f016
pythonCat <pythoncat@qq.com>
2019-05-08 13:31:28 +0800

    CLion自动编译命令探索

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 code for C Primer Plus (Sixth Edition)
 
 * IDE: CLION
+
+* CLion 中编译执行命令
+
+```bash
+mkdir -p cmake-build-debug
+cd cmake-build-debug
+cmake ../
+make
+```
+
+> 与执行`run.sh`效果相同
---
+++ run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+echo "start to build now!"
+echo "current project path:"$(pwd)
+PROJECT_NAME=$(basename `pwd`)
+echo "current project name:${PROJECT_NAME}"
+
+# 清空{PROJECT}/build
+build="cmake-build-debug"
+if [[ ! -d ${build} ]]
+then
+    mkdir ${build}
+else
+    rm -rf ${build}
+    mkdir ${build}
+fi
+# cmake and make
+cd ${build}
+cmake ../
+make
+echo "build finish!"
