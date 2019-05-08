fe9705f0f4d7c058db5c48eb6fa3f4ebb7a200a1
pythonCat <pythoncat@qq.com>
2019-05-08 22:26:40 +0800

    9.11.6

--- src/main.c
+++ src/main.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
 #include <ctype.h>
-// 将较大的一个重新赋值给两个变量
-void large_of(double * a, double * b);
-
+// 将3个数值从小到大重新赋值给3个变量
+void sort(double * a, double * b,double * c);
+double min(double a, double b, double c);
+double max(double a, double b, double c);
+double diff(double a,double b, double c, double d, double e);
 int main(void)
 {
-    printf("please input two double num: ");
-    double a,b;
+    printf("please input 3 double num: ");
+    double a,b,c;
     char ch;
-    while( 2 != scanf("%lf %lf", &a, &b))
+    while( 3 != scanf("%lf %lf %lf", &a, &b, &c))
     {
         while( (ch = getchar()) !='\n' )
             putchar(ch);
-        printf(" is not two valid num.\nplease try again: ");
+        printf(" is not 3 valid num.\nplease try again: ");
     }
-    printf("origin nums are %g,%g\n", a, b);
-    large_of(&a ,&b);
-    printf("after large nums are %g,%g\n", a, b);
+    printf("origin nums are %g,%g,%g\n", a, b, c);
+    sort(&a , &b, &c);
+    printf("after large nums are %g,%g,%g\n", a, b, c);
 }
 
-void large_of(double * a,double * b)
+void sort(double * a,double * b,double * c)
 {
-    double max = *a > *b ? *a : *b;
-    *a = max;
-    *b = max;
+    double min1 = min(*a, *b, *c);
+    double max1 = max(*a, *b, *c);
+    double t = diff(*a, *b, *c, min1,max1);
+
+    *a = min1;
+    *b = t;
+    *c = max1;
+}
+
+double min(double a, double b,double c)
+{
+    double x = a < b ? a : b;
+    return x < c ? x : c;
+}
+double max(double a, double b, double c)
+{
+    double xx = a > b ? a : b;
+    return xx > c ? xx : c;
+}
+double diff(double a, double b, double c, double d, double e)
+{
+    double k;
+    if( a != d && a != e )
+        k = a;
+    else if( b !=d && b != e )
+        k = b;
+    else
+        k = c;
+    return k;
 }
