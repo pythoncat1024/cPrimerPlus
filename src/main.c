ba60a459a1faeae238a492c23bc6c7bc67dba846
pythonCat <pythoncat@qq.com>
2019-05-08 22:03:47 +0800

    9.11.5

--- src/main.c
+++ src/main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
+// 将较大的一个重新赋值给两个变量
+void large_of(double * a, double * b);
 
-double avg(double a, double b);
 int main(void)
 {
-    printf("input two double num: ");
+    printf("please input two double num: ");
     double a,b;
     char ch;
-    while( 2 != scanf("%lf %lf", &a, &b) )
+    while( 2 != scanf("%lf %lf", &a, &b))
     {
-        while( (ch = getchar()) !='\n')
+        while( (ch = getchar()) !='\n' )
             putchar(ch);
         printf(" is not two valid num.\nplease try again: ");
     }
-    double rt = avg(a,b);
-    printf("the avg of %g,%g is %g\n", a, b, rt);
-    return 0;
+    printf("origin nums are %g,%g\n", a, b);
+    large_of(&a ,&b);
+    printf("after large nums are %g,%g\n", a, b);
 }
 
-double avg(double a,double b)
+void large_of(double * a,double * b)
 {
-    double x = 1/a;
-    double y = 1/b;
-    double z = (x+y) / 2;
-    return 1/z;
+    double max = *a > *b ? *a : *b;
+    *a = max;
+    *b = max;
 }
-
