06a09dda63c4d3ef3f457fb378ffa3ff505c8a79
pythonCat <pythoncat@qq.com>
2019-05-09 22:28:06 +0800

    关于'指向 const 指针'的说明
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
-#define SIZE 4
 
-int sum(int * arr, int n);
-int sum_arr(int * start, int * end);
 int main(void)
 {
-    int arr[SIZE] = {5, 6, 7, 8};
-    printf("sum of arr = %d\n", sum(arr, SIZE));
-    // c 编译器实现保证了 arr + SIZE 对应的地址可以访问，但是对应的值未定义！
-    printf("sum_arr = %d\n", sum_arr(arr,arr + SIZE));
-}
+    int zoo[23] = {};
+    int rain[4] = {1, 2};
+    const int * ptr = rain; // ptr 被称为：指向 const 指针
 
-int sum_arr(int * start, int * end)
-{
-    int total = 0;
-    while(start < end)
-    {
-        total += *start++;
-        // total += *start++; ===> total += *start; start++;
-    }
-    return total;
+    printf("ptr = %p, *ptr = %d\n", ptr, *ptr);
+    // ptr++;
+    ptr += 1; // ok
+    // 说明 指向const 指针可以被改变。
+    // *ptr = 66;  // 编译失败, const 指针不能去改变值
+    ptr = &zoo[0]; // ok 再次说明指向 const 指针可以被改变，只是不能改变值
+    printf("after ++ ptr = %p, *ptr = %d\n", ptr, *ptr);
+    return 0;
 }
-int sum(int * arr,int n)
-{
-    int total = 0;
-    for(int i = 0; i < n; i++)
-    {
-        total += *(arr + i);
-        // 指针+1，1对应的是一个元素长度
-    }
-    return total;
-}
-
