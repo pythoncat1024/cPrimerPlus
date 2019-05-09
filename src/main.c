b405a7420a7261615e67a437be255a33427dd196
pythonCat <pythoncat@qq.com>
2019-05-10 00:49:43 +0800

    二维数组指针操作

--- src/main.c
+++ src/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
-
+#define K 3
+void show(const int arr[][K],int row);
+void modify(int (*arr)[K],int row, int x);
 int main(void)
 {
-    int zoo[23] = {};
-    int rain[4] = {1, 2};
-    const int * ptr = rain; // ptr 被称为：指向 const 指针
-    int * const p = rain; // p 被称为：const 指针
-    p[1] = 32; // ok
-    // p = zoo; // 编译失败， const 指针，不能被二次赋值。
+    int rain[4][3] = {
+        {7,8,9}, {2,4,6}, {1,3,5}, {4,3,1}
+    };
+    printf("before modify:\n");
+    show(rain, 4);
+    modify(rain, 4, 10);
+    printf("after modify:\n");
+    show(rain, 4);
     return 0;
+}
+
+void modify(int (*arr)[K], int row, int x)
+{
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < K; j++){
+            // arr[i][j] += x;
+            *(* (arr + i)+j) += x;
+        }
+    }
+}
+
+void show(const int arr[][K], int row)
+{
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < K; j++){
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
 }
