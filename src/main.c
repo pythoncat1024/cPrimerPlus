85058f9e8084049f66ea5cfcc2467e52212335c2
pythonCat <pythoncat@qq.com>
2019-05-09 21:05:00 +0800

    array for count of rain

--- src/main.c
+++ src/main.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define YEAR 5
+#define MONTH 12
+#define MAX 100
 
-// 斐波那契数
-long fibonacci(long n);
+// 研究5年的每个月的降水量数据
 
-long r_fibonacci(long n);
-
-int main(void) {
-    printf("input a number to compute fibonacci num of this number: ");
-    long n;
-    while (1 != scanf("%ld", &n)) {
-        while (getchar() != '\n')
-            continue;
-        printf("illegal input!\nplease try again: ");
-    }
-    printf("%ld's r fibonacci number is %ld\n", n, r_fibonacci(n));
-    printf("%ld's fibonacci number is %ld\n", n, fibonacci(n));
-    return 0;
-}
-
-long fibonacci(long n) {
-    long d;
-    if (1 == n || 2 == n)
-        d = 1;
-    else {
-        long a = 1;
-        long b = 1;
-        for (long i = 3; i <= n; i++) {
-            d = b;
-            b = a + b;
-            a = d;
-            d = b; // ? ok ? ## ok!
+int main(void)
+{
+    float rain[YEAR][MONTH] = {};
+    for( int i = 0; i < YEAR ; i++ )
+    {
+        for( int j =0; j < MONTH; j++ )
+        {
+            // 通过 rand 函数给元素赋值
+            rain[i][j] = rand() % MAX;
         }
     }
-    return d;
-}
 
-long r_fibonacci(long n) {
-    if (n == 1 || n == 2)
-        return 1;
-    else
-        return r_fibonacci(n - 1) + r_fibonacci(n - 2);
+    float total_m = 0; // 每个月的总降水量
+    for(int m = 0; m < MONTH; m++)
+    {
+        total_m = 0;
+        for(int y = 0; y < YEAR; y++)
+        {
+            total_m += rain[y][m];
+        }
+
+        printf(" 第 %d 月的平均降水量为：%g\n", m+1, total_m / YEAR);
+    }
+    return 0;
 }
