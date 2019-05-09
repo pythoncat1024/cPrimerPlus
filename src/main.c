56e819380f91943f89053fdaa20f0baa7988ecd0
pythonCat <pythoncat@qq.com>
2019-05-09 22:34:24 +0800

    关于 'const指针' 的说明
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -5,13 +5,8 @@ int main(void)
     int zoo[23] = {};
     int rain[4] = {1, 2};
     const int * ptr = rain; // ptr 被称为：指向 const 指针
-
-    printf("ptr = %p, *ptr = %d\n", ptr, *ptr);
-    // ptr++;
-    ptr += 1; // ok
-    // 说明 指向const 指针可以被改变。
-    // *ptr = 66;  // 编译失败, const 指针不能去改变值
-    ptr = &zoo[0]; // ok 再次说明指向 const 指针可以被改变，只是不能改变值
-    printf("after ++ ptr = %p, *ptr = %d\n", ptr, *ptr);
+    int * const p = rain; // p 被称为：const 指针
+    p[1] = 32; // ok
+    // p = zoo; // 编译失败， const 指针，不能被二次赋值。
     return 0;
 }
