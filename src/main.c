b78f7d0161712cebd5238ec4d9b9e00929a17c6e
pythonCat <pythoncat@qq.com>
2019-05-11 09:26:57 +0800

    10.12.7

--- src/main.c
+++ src/main.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-    int (*ptr)[2];
-    int torf[2][2] = {12, 14, 16};
-    ptr = torf;
-    printf("**ptr ====== %d,  **(ptr+1) = %d\n", **ptr, **(ptr + 1));
-    printf("torf[0][0] = %d, torf[1][0] = %d\n", torf[0][0], torf[1][0]);
-    // 这里注意，如果 ptr 是一个二维数组，那么 **(ptr+1) == arr[1][0];
+    int digits[10];
+    float rates[6];
+    int mat[3][5];
+    int * psa[20];
+    char (*pstr)[20];
     return 0;
 }
