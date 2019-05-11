f5cd763d401c6221b9bf086279f9d37f0aa61665
pythonCat <pythoncat@qq.com>
2019-05-11 09:45:03 +0800

    10.12.12

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void handle_tr(double * trots, int size);
+void handle_cl(short (* ptr)[30], int row);
+void handle_sh(long (*ptr)[10][15], int x);
+
+void h_tr(int size, double * tr);
+void h_cl(int row, int size, short clops[][row]);
+
+void h_sh(int x,int y,int z, long shots[][y][z]);
 int main(void)
 {
-    int arr[6] = {1, 2, 4, 8, 16, 32};
-    int sm[100] = { [99] = -1 };
-    int st[100] = {[5] = 101, [10] = 101, [11] = 101, [12] = 101, [3] = 101 };
-
-    printf("st %d, %d, %d\n", st[5], st[12], st[3]);
+    double trots[20];
+    short clops[10][30];
+    long shots[5][10][15];
     return 0;
 }
