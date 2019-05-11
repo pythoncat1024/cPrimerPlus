ee592575c0910318b3d8e2e4a9b715eb3563e88f
pythonCat <pythoncat@qq.com>
2019-05-11 14:01:09 +0800

    10.13.14

--- src/main.c
+++ src/main.c
@@ -1,42 +1,76 @@
 #include <stdio.h>
+#include <ctype.h>
 
-void added(int (*arr)[5], int row);
-void show(const int (*arr)[5], int row);
+void input(int row,int col, double (*arr)[col]);
+void show(int row, int col, const double (*arr)[col]);
+double avg(const double * start, int length);
+double max(const double * start, int length);
 
 int main(void)
 {
-    int arr[3][5] = {
-        {1, 2, 3, 4, 5},
-        {2, 4, 6, 8, 10},
-        {1, 3, 5, 7, 9}
-    };
-    printf("show origin:\n");
-    show(arr, 3);
-    printf("show  added:\n");
-    added(arr, 3);
-    show(arr, 3);
+    double arr[3][5];
+    input(3, 5, arr);
+    printf("# arr :\n");
+    show(3, 5, arr);
+    double average = avg((double *) arr,3 * 5);
+    double m = max((double *) arr, 3 * 5);
+
+    printf("avg = %g, max = %g\n", average, m);
     return 0;
 }
 
-void added(int (*arr)[5], int row)
+double max(const double * start, int n)
 {
+    double max = *start;
+    const double * end = start + n;
+    while(start < end)
+    {
+        if(max < *start)
+        {
+            max = *start;
+        }
+        start++;
+    }
+    return max;
+}
+
+double avg(const double * start, int n)
+{
+    double total = 0;
+    const double * end = start + n;
+    while(start < end)
+    {
+        total += *start;
+        start++;
+    }
+    return total / n;
+}
+
+void input(int row, int col, double (*arr)[col])
+{
+    int ch;
     for(int i = 0; i < row; i++)
     {
-        for(int j = 0; j < 5; j++)
-        {
-            arr[i][j] *= 2;
-            *(*(arr +i) + j) *=1;
+        printf("请输入第%d组数字(每组%d个,回车结束输入)：", i+1, 5);
+       // 这样写死是5个了，处理方便一点
+       while( col != scanf("%lf %lf %lf %lf %lf",
+                   &arr[i][0], &arr[i][1], &arr[i][2],
+                   &arr[i][3], &arr[i][4]))
+       {
+           while( (ch = getchar()) != '\n')
+               putchar(ch);
+           printf(" is not valid input!\nPlease try again: ");
         }
     }
 }
 
-void show(const int (*arr)[5], int row)
+void show(int row,int col, const double (*arr)[col])
 {
     for(int i = 0; i < row; i++)
     {
-        for(int j = 0; j < 5; j++)
+        for(int j = 0; j < col; j++)
         {
-            printf("%4d", *(*(arr + i) + j));
+            printf("%5.1lf", arr[i][j]);
         }
         printf("\n");
     }
