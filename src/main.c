cf9749437dda3c31e8ffe0900a5701b0808bc22e
pythonCat <pythoncat@qq.com>
2019-05-11 09:38:21 +0800

    10.12.8

--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    int digits[10];
-    float rates[6];
-    int mat[3][5];
-    int * psa[20];
-    char (*pstr)[20];
+    int arr[6] = {1, 2, 4, 8, 16, 32};
+    int sm[100] = { [99] = -1 };
+    int st[100] = {[5] = 101, [10] = 101, [11] = 101, [12] = 101, [3] = 101 };
+
+    printf("st %d, %d, %d\n", st[5], st[12], st[3]);
     return 0;
 }
