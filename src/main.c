d36335238db9a0fa02fb6f785b2025c9764957ad
pythonCat <pythoncat@qq.com>
2019-05-11 09:22:11 +0800

    10.12.5

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int * ptr;
-    int torf[2][2] = {12, 14, 16, 18};
-    ptr = torf[0];
-    printf("*ptr = %d? , *(ptr+2) = %d? , %d\n", *ptr, *(ptr + 2), *(ptr + 3));
-    printf("(ptr + 2) = %p, &torf[1][0] = %p\n", (ptr + 2), &torf[1][0]); // 二者相等
-    // 从这里可以看出，二维数组在内存中跟一维数组是一样的，也是接着开辟内存的
+    int (*ptr)[2];
+    int torf[2][2] = {12, 14, 16};
+    ptr = torf;
+    printf("**ptr ====== %d,  **(ptr+1) = %d\n", **ptr, **(ptr + 1));
+    printf("torf[0][0] = %d, torf[1][0] = %d\n", torf[0][0], torf[1][0]);
+    // 这里注意，如果 ptr 是一个二维数组，那么 **(ptr+1) == arr[1][0];
     return 0;
 }
