f3bcbc12f258dfc90d5ffd9636aed04b4f3330be
pythonCat <pythoncat@qq.com>
2019-05-11 10:21:23 +0800

    10.13.3

--- src/main.c
+++ src/main.c
@@ -1,63 +1,35 @@
 #include <stdio.h>
 
-void copy_arr(double * target, const double * source, int size);
-void copy_ptr(double * target, const double * source, int size);
-void copy_ptrs(double * target, const double * start, const double * end);
-void show_arr(const double * start, const double * end);
-
+void oneline(int * arr, int n);
+int max(int * arr, int n);
 int main(void)
 {
-    double source[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    double target1[5];
-    double target2[5];
-    double target3[5];
-
-    copy_arr(target1, source, 5);
-    copy_ptr(target2, source, 5);
-    copy_ptrs(target3, source, source + 5);
-    printf("show     src: ");
-    show_arr(source, source + 5);
-    printf("show target1: ");
-    show_arr(target1, target1 + 5);
-    printf("show target2: ");
-    show_arr(target2, target2 + 5);
-    printf("show target3: ");
-    show_arr(target3, target3 + 5);
+    int arr[5] = {23, 12, 37, 4, 9};
+    int m = max(arr, 5);
+    printf("max in arr[");
+    oneline(arr, 5);
+    printf("] =  %d\n", m);
     return 0;
 }
-
-void copy_ptrs(double * target, const double * start,const double * end)
+void oneline(int * arr,int n)
 {
-    while(start < end)
+    for(int i = 0; i < n; i++)
     {
-        *target = *start;
-        target++;
-        start++;
+        if( i == n-1 )
+            printf("%4d", *(arr + i));
+        else
+            printf("%4d,", *(arr + i));
     }
 }
-
-void copy_ptr(double * target, const double * source, int size)
+int max(int * arr, int n)
 {
-    for(int i = 0; i < size; i++)
+    int max = arr[0];
+    for(int i = 0; i < n; i++)
     {
-        *(target + i) = *(source +i);
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
     }
-}
-
-void copy_arr(double * target, const double * source, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
-        target[i] = source[i];
-    }
-}
-
-void show_arr(const double * start, const double *end)
-{
-    while(start < end)
-    {
-        printf("%-8g", *start);
-        start++;
-    }
-    printf("\n");
+    return max;
 }
