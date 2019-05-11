cb1c76c43c02427abd6f37785f1011e7606ae561
pythonCat <pythoncat@qq.com>
2019-05-11 10:27:33 +0800

    10.13.4

--- src/main.c
+++ src/main.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 
-void oneline(int * arr, int n);
-int max(int * arr, int n);
+// 打印输出数组
+void oneline(double * arr, int n);
+// 返回最大元素下标
+int max_of_index(double *arr, int n);
 int main(void)
 {
-    int arr[5] = {23, 12, 37, 4, 9};
-    int m = max(arr, 5);
-    printf("max in arr[");
+    double arr[5] = {23.4, 12.4, 37.4, 4.4, 9.4};
+    int index = max_of_index(arr, 5);
+    printf("max ele index in arr[");
     oneline(arr, 5);
-    printf("] =  %d\n", m);
+    printf("] =  %d\n", index);
     return 0;
 }
-void oneline(int * arr,int n)
+int max_of_index(double * arr, int n)
 {
-    for(int i = 0; i < n; i++)
-    {
-        if( i == n-1 )
-            printf("%4d", *(arr + i));
-        else
-            printf("%4d,", *(arr + i));
-    }
-}
-int max(int * arr, int n)
-{
-    int max = arr[0];
+    double max = arr[0];
+    int index = 0;
     for(int i = 0; i < n; i++)
     {
         if(max < arr[i])
         {
             max = arr[i];
+            index = i;
         }
     }
-    return max;
+    return index;
 }
+void oneline(double * arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if( i == n-1 )
+            printf("%4g", *(arr + i));
+        else
+            printf("%4g,", *(arr + i));
+    }
+}
+
