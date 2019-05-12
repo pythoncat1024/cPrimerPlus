4e18ae1a311e609c4f0f614990f82bdb417c9175
pythonCat <pythoncat@qq.com>
2019-05-12 16:35:40 +0800

    fgets/fputs

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-#define SIZE 40
+#include <string.h>
+#define SIZE 10
 int main(void)
 {
     char line[SIZE];
     puts("请输入一句话:");
-    gets(line);
-    // gets(char *) 可以获取一个输入的字符串内容，但是对于长度没有控制，不安全
-    puts(line);
+    fgets(line, SIZE, stdin);
+    // fgets 如果输入小于 SIZE，则保留全部包括'\n'；最后是'\0'
+    // 如果输入大于 SIZE, 则SIZE-1 转换为 '\0'
+
+    fputs(line, stdout);
+    // fputs 不会在输出的末尾加上'\n', line 里面是什么就输出什么
+    printf("use printf: %s\n", line);
+    printf("length of line = %lu\n", strlen(line));
     return 0;
 }
