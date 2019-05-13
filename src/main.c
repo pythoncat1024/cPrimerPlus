35e6205868afd0f7bcfac17d3c776a16ea559ac4
pythonCat <pythoncat@qq.com>
2019-05-13 20:37:43 +0800

    sprintf
    sprintf 类似 strcpy 也是将结果拷贝到目标字符串。也没有长度检测。
    
    不过可以通过 %s,%d 这样的格式化字符串去组合拼接成想要的字符串格式
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 19
+#define SIZE 191
 
 int main(void)
 {
 
     char line[SIZE] = "hello world!";
-    char * two = "duck like java, but cat like python more";
-    printf("before#one=%s, two=%s\n", line, two);
-    // 字符串拷贝函数 相当于Java 中 String ss = str; // str 赋值给 ss
-    char * ret = strncpy(line, two, SIZE - 1);
-    printf("one=%s, two=%s, ret=%s\n", line, two, ret);
+    char * ptr = "duck like java more.";
+    printf("before:line = %s\n", line);
+    int ret = sprintf(line, "%s 哈哈#霓虹# %d , ptr=%p", ptr, 32, ptr);
+    printf("after sprintf:\nline = [%s], ret = %d\n", line, ret);
     return 0;
 }
