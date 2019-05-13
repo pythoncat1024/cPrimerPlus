efdeec7cdde214f5ba4dacbc130c0138dd45f2ab
pythonCat <pythoncat@qq.com>
2019-05-13 21:48:47 +0800

    strchr
    该函数的返回值是一个 char * . 比如 char * s = strchr("ABC", 'B');
    printf("%s\n",s); --> 则输出[BC] . 如果没有找到，返回 NULL
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@ int main(void)
 
     char line[SIZE] = "hello world!";
     char * ptr = "duck like java more.";
-    printf("before:line = %s\n", line);
-    int ret = sprintf(line, "%s 哈哈#霓虹# %d , ptr=%p", ptr, 32, ptr);
-    printf("after sprintf:\nline = [%s], ret = %d\n", line, ret);
+    char ch = 'e';
+    char * index = strchr(line, ch);
+    char * index2 = strchr(ptr, ch);
+    printf("index = %s, index2 = %s\n", index, index2);
+    char * ss = strchr("duck", 'A');
+    printf(" find 'A' in \"duck\" = %s\n", ss);
     return 0;
 }
