38f857ed67235dea14e1d9851cfa921d284a5454
pythonCat <pythoncat@qq.com>
2019-05-13 20:27:26 +0800

    strcpy

--- src/main.c
+++ src/main.c
@@ -6,8 +6,10 @@ int main(void)
 {
 
     char line[SIZE] = "hello world!";
-    char * two = "Hello world!";
-    int ret = strcmp(line, two);
-    printf("one=%s, two=%s, ret=%d\n", line, two, ret);
+    char * two = "duck like java, but cat like python more";
+    printf("before#one=%s, two=%s\n", line, two);
+    // 字符串拷贝函数 相当于Java 中 String ss = str; // str 赋值给 ss
+    char * ret = strcpy(line, two);
+    printf("one=%s, two=%s, ret=%s\n", line, two, ret);
     return 0;
 }
