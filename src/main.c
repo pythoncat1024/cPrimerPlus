35d9a0c94e80b335b9e8a8ae6954bc7a9cdb2442
pythonCat <pythoncat@qq.com>
2019-05-13 20:15:28 +0800

    strncat

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#define SIZE 109
+#include <string.h>
+#define SIZE 191
 
 int main(void)
 {
 
     char line[SIZE] = "hello world!";
+    char * two = " don't touch me!";
+    printf("before strncat. one=%s, two=%s\n", line, two);
+    // 字符串拼接函数
+    char * ret = strncat(line, two, SIZE - strlen(two));
 
-    puts(line);
-    puts(line + 4); // 字符串处理函数都可以这样改变处理位置
+    printf("one=%s, two=%s, ret=%s\n", line, two, ret);
     return 0;
 }
