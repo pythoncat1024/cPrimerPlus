18d01a138d89386f53c4c714fd04b22ec9ae2462
pythonCat <pythoncat@qq.com>
2019-05-13 20:30:22 +0800

    strncpy

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 191
+#define SIZE 19
 
 int main(void)
 {
@@ -9,7 +9,7 @@ int main(void)
     char * two = "duck like java, but cat like python more";
     printf("before#one=%s, two=%s\n", line, two);
     // 字符串拷贝函数 相当于Java 中 String ss = str; // str 赋值给 ss
-    char * ret = strcpy(line, two);
+    char * ret = strncpy(line, two, SIZE - 1);
     printf("one=%s, two=%s, ret=%s\n", line, two, ret);
     return 0;
 }
