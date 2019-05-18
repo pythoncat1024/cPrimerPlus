2d87afdbdf06d3c415578114d4fa912acdca8cce
pythonCat <pythoncat@qq.com>
2019-05-18 19:53:25 +0800

    int main(int argc, char * argv[]);
    ./a.out arg1 arg2
    
    命令行参数

--- src/main.c
+++ src/main.c
@@ -1,64 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
-#define SIZE 5
+int main(int argc, char * argv[]) {
 
-void show(int size, const char *str[]);
-
-void copy_ptr(int size, char *dst[], char *src[]);
-
-void sort_str(int size, char *dst[]);
-
-int main(void) {
-
-    char *strings[SIZE] = {
-            "100", "300", "500", "200", "400",
-    };
-
-    char *dst[SIZE];
-    printf("show origin:\n");
-    show(SIZE, (const char **) strings);
-    printf("show copied:\n");
-    copy_ptr(SIZE, dst, strings);
-    show(SIZE, (const char **) dst);
-
-    printf("show sorted:\n");
-    sort_str(SIZE, dst);
-    show(SIZE, (const char **) dst);
+    printf("参数个数：%d\n", argc);
+    for(int i = 0; i < argc; i++) {
+        printf("%s\t", argv[i]);
+    }
+    printf("\n");
     return 0;
 }
-
-/**
- * 算法实现有问题
- * @param size size
- * @param arr arr
- */
-void sort_str(int size, char *arr[]) {
-    int cmp;
-    char *s;
-    int temp; // 定义一个临时变量
-    for (int i = 0; i < size - 1; i++) {//冒泡趟数
-        for (int j = 0; j < size - i - 1; j++) {
-            cmp = strcmp(arr[j + 1], arr[j]);
-            if (cmp) {
-                s = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = s;
-            }
-            if (j == 1)break;
-        }
-    }
-}
-
-void copy_ptr(int size, char *dst[], char *src[]) {
-    for (int i = 0; i < size; ++i) {
-        dst[i] = src[i];
-    }
-}
-
-void show(int size, const char *str[]) {
-    for (int i = 0; i < size; ++i) {
-        printf("%s\n", *(str + i));
-    }
-}
-
