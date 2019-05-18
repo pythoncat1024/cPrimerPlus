b2ae1809309806d405ff2d9a72c79fe7128fc901
pythonCat <pythoncat@qq.com>
2019-05-18 20:40:13 +0800

    11.12.2

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char * argv[]) {
 
-    char * end;
-    long num;
-    printf("参数个数：%d\n", argc);
-    for(int i = 0; i < argc; i++) {
-        num = strtol(argv[i], &end, 10);
-        printf("arg[%d] = %s,%ld, end with:%s\n", i, argv[i], num, end);
-    }
-    printf("\n");
+    char note[] = "See you at the snack bar.";
+    char * ptr = note;
+    puts(ptr);
+    puts(++ptr);
+    note[7] = '\0';
+    puts(note);
+    puts(++ptr);
     return 0;
 }
