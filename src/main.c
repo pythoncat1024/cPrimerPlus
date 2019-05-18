7470a31ad7dac1ced430cfacf951d1ef34d62f0f
pythonCat <pythoncat@qq.com>
2019-05-18 20:21:20 +0800

    字符串转数字
    double num = strtod(char * str, char ** end); // 只能十进制
    long num = strtol(char * str, char ** end, int base);
    其实 str 是待转换的字符串
         end 是转换结束的字符指针的地址（类似 scanf("%d", &num) 一样，end
         被函数赋值）
         base 是进制数，比如 2,8,10,16 这些进制
    
    call sample:
        char * end;
        long num = strtol("1234abc", &end, 10);
    result sample:
        num == 1234;
        *end == 'a'; // end 成为指向 "abcd"的首字符地址的指针了

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char * argv[]) {
 
+    char * end;
+    long num;
     printf("参数个数：%d\n", argc);
     for(int i = 0; i < argc; i++) {
-        printf("%s\t", argv[i]);
+        num = strtol(argv[i], &end, 10);
+        printf("arg[%d] = %s,%ld, end with:%s\n", i, argv[i], num, end);
     }
     printf("\n");
     return 0;
