cf46600ea8d2041898d2e9f9bd66ad8f7e0e90dc
pythonCat <pythoncat@qq.com>
2019-05-19 19:26:20 +0800

    11.13.10
    drop_space(char * src); // 丢弃原字符串中的空格

--- src/main.c
+++ src/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "../include/utils.h"
+#define SIZE 40
+char * drop_space(char * src);
 
 int main(int argc, char * argv[]) {
-
-    char str[] = "hello world!";
-    printf("origin:%s\n", str);
-    char *ss = strrevert(str);
-    printf("reverted:%s,%s\n", str, ss);
+    char src[SIZE];
+    printf("input lines:\n");
+    while(s_gets(src, SIZE)) {
+        printf("what you input = [%s]\n", src);
+        drop_space(src);
+        printf("drop sapce = [%s]\n", src);
+    }
+    print_some();
     return 0;
+}
+
+char * drop_space(char * src) {
+    int n = strlen(src);
+    for(int i = 0; i < n; i++) {
+        n = strlen(src);
+        if(isspace(src[i])) {
+            for(int k = i; k < n -1; k++) {
+                src[k] = src[k+1];
+            }
+            src[n-1] = '\0';
+        }
+    }
+    return src;
 }
