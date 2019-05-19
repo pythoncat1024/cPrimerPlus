dc88e57f4a0c63271ba797decb6e72e2eb7138b3
pythonCat <pythoncat@qq.com>
2019-05-19 22:53:26 +0800

    11.13.13
    main(int argc, char *argv[]); 对 argv 的使用

--- src/main.c
+++ src/main.c
@@ -1,35 +1,10 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include "../include/utils.h"
-#define MAX 300
-int main(void) {
-    char line[MAX];
-    printf("input anything:\n");
-    s_gets(line, MAX);
-    size_t len = strlen(line);
-    int up = 0;
-    int low = 0;
-    int word = 0;
-    int num = 0;
-    char prev = '\n';
-    for(int i = 0; i < len; i++) {
-        if(isnumber(line[i])) {
-            num++;
-        }
-        if(isupper(line[i])) {
-            up++;
-        }
-        if(islower(line[i])) {
-            low++;
-        }
-        if(!isalpha(prev) && isalpha(line[i])) {
-            word++;
-        }
-        prev = line[i];
-    }
 
-    printf("word=%d,up=%d,low=%d,number=%d\n",
-            word, up, low, num);
+int main(int argc, char * argv[]) {
+
+    printf("revert args\n");
+    for(int i = argc - 1; i > 0; i--) {
+        printf("%s\t", argv[i]);
+    }
     return 0;
 }
