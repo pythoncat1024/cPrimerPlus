cc1c3f4aa21000b4167df08d67aa4e67d2e84786
pythonCat <pythoncat@qq.com>
2019-05-19 23:14:48 +0800

    11.13.16

--- src/main.c
+++ src/main.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, char * argv[]) {
-
-    if(argc -1 < 2)
-        puts("count of args must >= 2");
-    else {
-        char * end;
-        double d = strtod(argv[1], &end);
-        long b = strtol(argv[2], &end, 10);
-        double r = pow(d,b);
-        printf("arg1 = %g, arg2 = %ld, result = %g\n", d, b, r);
+    printf("input multi lines:\n");
+    char str[1000];
+    char ch;
+    int pos;
+    int menu;
+    if( argc == 1 )
+        menu = 0;
+    else if(strcmp("-p", argv[1]) == 0)
+        menu = 0;
+    else if(strcmp("-u", argv[1]) == 0)
+        menu = 1;
+    else if(strcmp("-l", argv[1]) == 0)
+        menu = 2;
+    else
+        menu = 0;
+    switch(menu) {
+        case 0:
+            while((ch = getchar()) != EOF && pos < 1000) {
+                str[pos] = ch;
+                pos++;
+            }
+            puts("###################");
+            printf("%s\n", str);
+            break;
+        case 1:
+            while((ch = getchar()) != EOF && pos < 1000) {
+                str[pos] = toupper(ch);
+                pos++;
+            }
+            puts("###################");
+            printf("%s\n", str);
+            break;
+        case 2:
+            while((ch = getchar()) != EOF && pos < 1000) {
+                str[pos] = tolower(ch);
+                pos++;
+            }
+            puts("###################");
+            printf("%s\n", str);
+            break;
     }
     return 0;
 }
