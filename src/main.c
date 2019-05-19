bf339a1fbcc2f3efefdac2488b6e985da6c5f687
pythonCat <pythoncat@qq.com>
2019-05-19 10:25:38 +0800

    11.13.7

--- src/main.c
+++ src/main.c
@@ -2,29 +2,40 @@
 #include <string.h>
 #include "../include/utils.h"
 
-const char * fstrchr(const char * str, const char key);
+char * mstrncpy(char * s1, char * s2, int n);
+void to_empty(char * arr, int n);
+
 int main(int argc, char * argv[]) {
-    printf("input what you want. empty line to quit:\n");
-    char arr[56];
-    char key;
-    const char * find;
-    while(s_gets(arr, 56) && 1 == scanf("%c", &key)) {
-        while(getchar() !='\n')
-            continue;
-        find = fstrchr(arr, key);
-        printf("arr=[%s],key=[%c],find=[%s]\n", arr, key, find);
+    printf("input what you want, empty line to quit::\n");
+    char s1[50], s2[50];
+    while(1) {
+        to_empty(s1, 50);
+        to_empty(s2, 50);
+        printf("input first line:\n");
+        char * p1 = s_gets(s1, 50);
+        if(!p1) break;
+        puts("input second line:");
+        char * p2 = s_gets(s2, 50);
+        if(!p2) break;
+        int n = 50 - strlen(p1);
+        char * p3 = mstrncpy(s1, s2, n);
+        printf("s1=[%s],s2=[%s],p3=[%s]\n", s1, s2, p3);
     }
-    puts("Done.");
+    puts("Done");
     return 0;
 }
-
-const char * fstrchr(const char * str, const char key) {
-    while(*str) {
-        if(*str == key)
-            return str;
-        str++;
+void to_empty(char * str, int n) {
+    for(int i = 0; i < n; i++) {
+        str[i] = '\0';
     }
-    return (const char *)NULL;
-
 }
+char * mstrncpy(char * s1, char * s2, int n) {
 
+    int pos = 0;
+    int start = strlen(s1);
+    while(*(s2+ pos) && pos < n) {
+        *(s1 + start + pos) = *(s2 + pos);
+        pos++;
+    }
+    return s1;
+}
