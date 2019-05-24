3e6604d48ab308221f4113ebde02e58c22437461
pythonCat <pythoncat@qq.com>
2019-05-24 21:21:08 +0800

    copy file
    strncat(dst, src, len); // -> len 必须大于等于 strlen(src) +1;!!!

--- src/main.c
+++ src/main.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char * argv[]) {
+#define LEN 14
+#define EXT ".cp" // 后缀名
 
+int main(int argc, char *argv[]) {
+    fprintf(stdout, "copy a file:\n");
+
+    char name[LEN];
     int ch;
     long count = 0;
-    FILE * fp;
-    if(argc < 2) {
+    FILE *fp; // origin file
+    FILE *fout; // origin.cp file
+    if (argc < 2) {
         printf("I need a file path in args!\n");
         exit(EXIT_FAILURE);
-    } else if((fp = fopen(argv[1],"r")) == NULL) {
+    } else if ((fp = fopen(argv[1], "r")) == NULL) {
         printf("%s is not a file!\n", argv[1]);
     } else {
-        while((ch = getc(fp)) != EOF) {
-            // putc(ch, stdout);
-            count++;
+        int name_OK = 0;
+        if (strncpy(name, argv[1], LEN - strlen(EXT))) {
+            if (strlen(name) > LEN - strlen(EXT)) {
+                name[LEN - (strlen(EXT) + 1)] = '\0';
+            }
+            size_t size = LEN - strlen(name) + 1;
+            if (strncat(name, EXT, size)) {
+                name_OK = 1;
+            }
+        }
+        if (!name_OK) {
+            fprintf(stderr, "define output file name %s fail.\n", name);
+            exit(EXIT_FAILURE);
+        }
+        if ((fout = fopen(name, "w")) == NULL) {
+            fprintf(stderr, "create file %s fail.\n", name);
+            exit(EXIT_FAILURE);
+        }
+
+        while ((ch = getc(fp)) != EOF) {
+            putc(ch, fout);
+            // count++;
         }
 
         if (0 != fclose(fp)) {
-            printf("Error in close %s\n", argv[1]);
-        }else {
-            printf("file %s has %ld count chars.\n", argv[1], count);
+            fprintf(stderr, "Error in close %s\n", argv[1]);
+        } else if (0 != fclose(fout)) {
+            fprintf(stderr, "Error in close %s\n", name);
+        } else {
+            fprintf(stdout, "src file [%s] \ncopied to \ndst file [%s]\n", argv[1], name);
         }
     }
     return 0;
