15b26878a445b97792fd364615a013e29cdf48d6
pythonCat <pythoncat@qq.com>
2019-05-25 21:31:08 +0800

    13.10.5
    grep -n key path

--- src/main.c
+++ src/main.c
@@ -1,57 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/utils.h"
 
-// 随机访问
 int main(int argc, char * argv[]) {
-   const int size = 108;
-   // create a double array ,which size equal size
-    const char * bf_name = "number.dat"; // binary file
-
-    double array[size];
-    // init array first
-    for(int i = 0; i < size; i++) {
-        array[i] = 100 * i + 1.0 * i / 100;
-    }
-    // write the array data to binary file
-    FILE * dst;
-    if((dst = fopen(bf_name, "wb")) == NULL) {
-        fprintf(stderr, "can not create file [%s]\n", bf_name);
+    if(argc != 3) {
+        fprintf(stderr, "need one key and one path, like grep -n key file.txt\n");
         exit(EXIT_FAILURE);
     }
-    // write data to file
-    if( size != fwrite(array, sizeof(double), size, dst)) {
-        fprintf(stderr,"### error appeared when write array to file [%s] done.\n", bf_name);
+    const char * key = argv[1];
+    const char * fname = argv[2];
+    FILE * fp;
+    if((fp = fopen(fname, "r")) == NULL) {
+        fprintf(stderr, "[%s] is not a file\n", fname);
         exit(EXIT_FAILURE);
     }
-    fclose(dst);
-    if((dst = fopen(bf_name, "rb")) == NULL) {
-        fprintf(stderr, "error appeared when open file [%s]\n", bf_name);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("which pos data you want to see [0, %d-1]:\n", size);
-    int pos;
-    int ch;
-    while(1 != scanf("%d", &pos) || pos < 0 || pos > size) {
-        while((ch = getchar()) != '\n')
-            putchar(ch);
-        puts(" is not a valid position. please try again");
-    }
-    while(getchar() != '\n')
-        continue;
-    double ele;
-    if(0 == fseek(dst, sizeof(double) * pos, SEEK_SET)) {
-        if( 1 == fread(&ele, sizeof(double), 1, dst)) {
-            printf("sscceed in access array[%d]! value =  %g\n", pos, ele);
-        } else {
-            fprintf(stderr, "error appeared when access array[%d]\n", pos);
-            exit(EXIT_FAILURE);
+    const int max_line = 1024 * 10;
+    char line[max_line];
+    int row = 0;
+    while(fgets(line, max_line, fp)) {
+        row++;
+        if(strstr(line, key)) {
+            printf("%d:", row);
+            fputs(line, stdout);
         }
-    } else {
-        fprintf(stderr, "error occoured when seek pointer to %d in file [%s]\n",
-                pos, bf_name);
     }
 
     return 0;
