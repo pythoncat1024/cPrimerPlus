14b368d26f900f931b7ef756ee0ae0ac6186684d
pythonCat <pythoncat@qq.com>
2019-05-29 21:27:17 +0800

    14.17.5

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct month {
     char name[18];
@@ -6,8 +7,11 @@ struct month {
     int days;
     int index;
 };
+typedef struct month m;
+int sum(int month_index, const m * arr);
+
 int main(void) {
-    struct month month_arr[12] = {
+    m month_arr[12] = {
         {"January", "Jan", 31, 1},
         {"February", "Feb", 28, 2},
         {"March", "Mar", 31, 3},
@@ -21,6 +25,22 @@ int main(void) {
         {"November", "Nov", 30, 11},
         {"December", "Dec", 31, 12}
     };
-
+    printf("please input a number [1,12]: ");
+    int num;
+    scanf("%d", &num);
+    if(num < 1 || num > 12) {
+        printf("Error input!\n");
+        exit(EXIT_FAILURE);
+    }
+    int total = sum(num, month_arr);
+    printf("total days sum from 1 to %d = %d\n", num, total);
     return 0;
+}
+
+int sum(int month_index, const m * arr) {
+    int total = 0;
+    for(int i = 0; i <= month_index - 1; i++) {
+        total += arr[i].days;
+    }
+    return total;
 }
