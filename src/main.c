bbe8bdbf04f7edd423a50234340e368979091f49
pythonCat <pythoncat@qq.com>
2019-05-29 21:15:05 +0800

    14.17.4

--- src/main.c
+++ src/main.c
@@ -7,6 +7,20 @@ struct month {
     int index;
 };
 int main(void) {
-    struct month m;
+    struct month month_arr[12] = {
+        {"January", "Jan", 31, 1},
+        {"February", "Feb", 28, 2},
+        {"March", "Mar", 31, 3},
+        {"April", "Apr", 30, 4},
+        {"May", "May", 31, 5},
+        {"June", "Jun", 30, 6},
+        {"July", "Jul", 31, 7},
+        {"August", "Aug", 31, 8},
+        {"September", "Sep", 30, 9},
+        {"October", "Oct", 31, 10},
+        {"November", "Nov", 30, 11},
+        {"December", "Dec", 31, 12}
+    };
+
     return 0;
 }
