d6968f7301ba5d730792cd3f56eb11ff7e97e072
pythonCat <pythoncat@qq.com>
2019-05-29 21:36:46 +0800

    14.17.6.a
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-struct month {
-    char name[18];
-    char nick[3];
-    int days;
-    int index;
-};
-typedef struct month m;
-int sum(int month_index, const m * arr);
+typedef struct lens {
+    float foclen;
+    float fstop;
+    char brand[30];
+} LENS;
+
+void inflate(LENS * arr, int size);
+void show(LENS *arr, int size);
+void print(LENS s);
 
 int main(void) {
-    m month_arr[12] = {
-        {"January", "Jan", 31, 1},
-        {"February", "Feb", 28, 2},
-        {"March", "Mar", 31, 3},
-        {"April", "Apr", 30, 4},
-        {"May", "May", 31, 5},
-        {"June", "Jun", 30, 6},
-        {"July", "Jul", 31, 7},
-        {"August", "Aug", 31, 8},
-        {"September", "Sep", 30, 9},
-        {"October", "Oct", 31, 10},
-        {"November", "Nov", 30, 11},
-        {"December", "Dec", 31, 12}
-    };
-    printf("please input a number [1,12]: ");
-    int num;
-    scanf("%d", &num);
-    if(num < 1 || num > 12) {
-        printf("Error input!\n");
-        exit(EXIT_FAILURE);
-    }
-    int total = sum(num, month_arr);
-    printf("total days sum from 1 to %d = %d\n", num, total);
+    LENS arr[10];
+    inflate(arr, 10);
+    show(arr, 10);
     return 0;
 }
-
-int sum(int month_index, const m * arr) {
-    int total = 0;
-    for(int i = 0; i <= month_index - 1; i++) {
-        total += arr[i].days;
+void inflate(LENS * arr, int size) {
+    for(int i = 0; i < size; i++) {
+        arr[i].foclen = 500;
+        arr[i].fstop = (float)1/2.0f;
+        strncpy(arr[i].brand, "Remarkata", 30);
     }
-    return total;
+}
+
+void show(LENS * arr, int size) {
+    for(int i = 0; i < size; i++) {
+        print(arr[i]);
+    }
+}
+
+void print(LENS s) {
+    printf("len=%g,op=%g,brand=%s\n", s.foclen, s.fstop, s.brand);
 }
