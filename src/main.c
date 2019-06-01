9e3b26cd4732ebd3bf4be52c797ffd487d20b99a
pythonCat <pythoncat@qq.com>
2019-06-01 17:08:55 +0800

    14.17.8
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-struct name {
-    char first[20];
-    char last[20];
+struct fullname {
+    char fname[20];
+    char lname[20];
 };
-struct bem {
-    int limbs;
-    struct name title;
-    char type[30];
+struct bard {
+    struct fullname name;
+    int born;
+    int died;
 };
 
-void show(struct bem bb);
-void print(struct bem * pb);
 int main(void) {
-
-    struct bem * pb;
-    struct bem deb = {
-        6, {"Berbnazel", "Gwolkpwolk"}, "Arcturan"
-    };
-    pb = &deb;
-    show(deb);
-    print(pb);
+    struct bard willie;
+    struct bard * pt = &willie;
+    printf("set born:");
+    scanf("%d", &willie.born);
+    printf("set born again:");
+    scanf("%d", &(pt->born));
+    printf("set lastname:");
+    scanf("%s", willie.name.lname);
+    printf("set lastname:");
+    scanf("%s", pt->name.lname);
+    char three = pt->name.lname[2];
+    size_t len = strlen(pt->name.fname) + strlen(pt->name.lname);
+    printf("three = %c, len = %zd\n", three, len);
     return 0;
-}
-void print(struct bem * bb) {
-    printf("%s %s is a %d-limbed %s\n", bb->title.first, bb->title.last, bb->limbs, bb->type);
-}
-void show(struct bem bb) {
-    printf("%s %s is a %d-limbed %s\n", bb.title.first, bb.title.last, bb.limbs, bb.type);
 }
