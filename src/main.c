775fa884da0efc44eff637e90904de010b420457
pythonCat <pythoncat@qq.com>
2019-06-01 17:15:22 +0800

    14.17.11 enum 定义和使用
    enum animail {cat, duck, pig, dog};
    enum animal nm = duck;
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-struct fullname {
-    char fname[20];
-    char lname[20];
-};
-struct bard {
-    struct fullname name;
-    int born;
-    int died;
-};
-
-int main(void) {
-    struct bard willie;
-    struct bard * pt = &willie;
-    printf("set born:");
-    scanf("%d", &willie.born);
-    printf("set born again:");
-    scanf("%d", &(pt->born));
-    printf("set lastname:");
-    scanf("%s", willie.name.lname);
-    printf("set lastname:");
-    scanf("%s", pt->name.lname);
-    char three = pt->name.lname[2];
-    size_t len = strlen(pt->name.fname) + strlen(pt->name.lname);
-    printf("three = %c, len = %zd\n", three, len);
+int main(int argc, char * argv[]) {
+    enum choies {no = 0, yes, maybe};
+    enum choies chi = maybe;
+    printf("enum chi  = %d\n", chi);
     return 0;
 }
