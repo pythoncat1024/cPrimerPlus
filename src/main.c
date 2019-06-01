f4777a7b892d9d93e6489ee1ca9e97fa12597478
pythonCat <pythoncat@qq.com>
2019-06-01 17:51:48 +0800

    14.17.13
    定义函数指针的数组
    
    double func(double a, double b);
    
    double (*pf)(double, double) = func;
    
    double(*pfarr[2])(double, double) = {func, pf};
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-char * method(char * src, char key);
-int main(int argc, char * argv[]) {
+#include <stdlib.h>
 
-    // 声明一个函数指针 pf
-    char * (*pf)(char * src, char key);
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+int main(int argc, char * argv[]) {
+    double (*pa)(double,double);
+
+    // 定义函数指针数组
+    double (*parr[4])(double,double) = {add, subtract, multiply, divide};
+    double added = parr[0](10, 2);
+    double multi = (*parr[2])(10, 2);
+    printf("added = %g, multi = %g, %d\n", added, multi, parr[3] == divide);
     return 0;
+}
+
+double divide(double a, double b) {
+    if(b == 0) {
+        fprintf(stderr, "divider can not be zero!\n");
+        exit(EXIT_FAILURE);
+    }
+    return a / b;
+}
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+double add(double a, double b) {
+    return a + b;
 }
