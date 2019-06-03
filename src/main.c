9a26671ce55f05501e005201903809e9994e0687
pythonCat <pythoncat@qq.com>
2019-06-03 20:18:13 +0800

    目前只是把数据读出来并显示出来了而已

--- src/main.c
+++ src/main.c
@@ -1,45 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct s_name {
-    char first[20];
-    char last[20];
-};
+#define LEN 1024 * 10
 
-struct student {
-    struct s_name name;
-    float grade[3];
-    float average;
-};
+void fail() { exit(EXIT_FAILURE); }
 
-void set_info(int size, struct student arr[]);
-void show_info(int size, struct student arr[]);
-int main(void) {
-    const int len = 1;
-    struct student students[len];
-    set_info(len, students);
-    show_info(len, students);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "I need a file path!\n");
+        fail();
+    } else {
+        char *path = argv[1];
+        FILE *fp = fopen(path, "r");
+        if (NULL == fp) {
+            fprintf(stderr, "%s is not a file path!\n", path);
+            fail();
+        } else {
+            if (0 != setvbuf(fp, NULL, _IOFBF, LEN)) {
+                fprintf(stderr, "error to set buf for file %s\n", path);
+                fail();
+            }
+            size_t read;
+            static char buff[LEN];
+            while ((read = fread(buff, sizeof(char), LEN, fp)) > 0) {
+                fwrite(buff, sizeof(char), read, stdout);
+            }
+            fclose(fp);
+        }
+    }
     return 0;
-}
-
-void show_info(int size, struct student arr[]) {
-    for(int i = 0; i < size; i++) {
-        printf("%-20s %-20s [%-4.1f, %-4.1f, %-4.1f] %4.1f\n",
-                arr[i].name.first, arr[i].name.last,
-                arr[i].grade[0], arr[i].grade[1], arr[i].grade[2],
-                arr[i].average);
-    }
-}
-
-void set_info(int size, struct student arr[]) {
-    for(int i = 0; i < size; i++) {
-        struct student *stu = &arr[i];
-        printf("%d/%d: input first name: ", i+1, size);
-        scanf("%20s", stu->name.first);
-        printf("%d/%d input last name: ", i+1, size);
-        scanf("%20s", stu->name.last);
-        printf("%d/%d input 3 grades: ", i+1, size);
-        scanf("%f %f %f", &(stu->grade[0]), &(stu->grade[1]), &(stu->grade[2]));
-        stu->average = (stu->grade[0] + stu->grade[1] + stu->grade[2]) / 3;
-    }
 }
