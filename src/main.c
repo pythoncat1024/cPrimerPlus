f72f8bcc4c36633a0254e0d351421118c6f0dbe9
pythonCat <pythoncat@qq.com>
2020-12-01 23:07:20 +0800

    1.2 将任意长度的多行输入打印到控制台,前面加上行号

--- src/main.c
+++ src/main.c
@@ -1,74 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MAX_COLUMNS 20
-#define MAX_INPUT 100
-
-int read_columns(int col[], int max);
-void rearrange(char input[], char output[], int col_len, int col[]);
+#define LINE '\n'
 
 int main(int argc, char *argv[])
 {
-    int n_columns;
-    int columns[MAX_COLUMNS];
-    char inputs[MAX_INPUT];
-    char outputs[MAX_INPUT];
-    n_columns = read_columns(columns, MAX_COLUMNS);
-
-    while (gets(inputs) != NULL)
-    {
-        printf("origin input: %s\n", inputs);
-        rearrange(inputs, outputs, n_columns, columns);
-        printf("rearranged line: %s\n", outputs);
-    }
-
-    return 0;
-}
-
-void rearrange(char input[], char output[], int col_len, int col[])
-{
-    int pou = 0;
-    unsigned long sin = strlen(input);
-    // output[0] = '\n';
-    for (int pos = 0; pos < col_len; pos += 2)
-    {
-        int begin = col[pos];
-        unsigned long len = col[pos + 1];
-        if (sin < begin || pou >= MAX_INPUT - 1)
-        {
-            // 如果 input 的长度小于开始拷贝的值
-            // 或者 output 没有空间了
-            break;
-        }
-        // 如果 要复制的长度超出 input的实际长度，就截取到尾部即可
-        if (begin + len > sin)
-        {
-            len = sin - begin;
-        }
-
-        strncpy(output + pou, input + begin, len);
-        pou += len;
-    }
-    output[pou] = '\0';
-}
-
-int read_columns(int col[], int max)
-{
+    printf("%s\n", "input any long lines,you can:");
     int ch;
-    int pos = 0;
-    while (scanf("%d", &col[pos]) == 1 && col[pos] >= 0)
+    int curr = 1, last = 0;
+    while ((ch = getchar()) != EOF)
     {
-        pos++;
+        if (ch == LINE)
+        {
+            curr += 1;
+        }
+        else
+        {
+            if (curr > last)
+            {
+                printf("\t%d: ", curr);
+            }
+            if (last != curr)
+            {
+                last = curr;
+            }
+        }
+        putchar(ch);
     }
 
-    if (pos % 2 != 0)
-    {
-        printf("error, pos must be even\n");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((ch = getchar()) != EOF && ch != '\n')
-        ;
-    return pos;
+    return EXIT_SUCCESS;
 }
+
+// fffhhhh\n
+// 1fffhhhh\n
+// dsdsddsd\n
+// 2
