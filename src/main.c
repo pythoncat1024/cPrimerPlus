e4e14f692d5db89995be0f5dc937182a322045b4
pythonCat <pythoncat@qq.com>
2020-12-07 21:43:55 +0800

    3.n q15 作用域与存储类型

--- src/main.c
+++ src/main.c
@@ -1,80 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-#define LEFT '{'
-#define RIGHT '}'
-
-int match(int a, int b)
+char b = 2;
+void y()
 {
-    return a == b;
+    // printf("%d\n", a); // error
+    printf("b = %d\n", b);
 }
-
-void check(char *str)
+a = 1;
+void x()
 {
-    int len = strlen(str);
-    int ch;
-    int f = 0, s = 0;
-    int stopLeftcollect = 0;
-    int earlyFail = 0;
-    for (int i = 0; i < len; ++i)
-    {
-        ch = str[i];
-        if (LEFT == ch)
-        {
-            if (s > 0)
-            {
-                if (s <= f)
-                {
-                    f = 1 + (f - s);
-                    stopLeftcollect = 0;
-                    s = 0;
-                }
-                else
-                {
-                    printf("======= current line braces match FAIL#\n");
-                    earlyFail = 1;
-                    break;
-                }
-            }
-            else
-            {
-                if (!stopLeftcollect)
-                {
-                    f++;
-                }
-            }
-        }
-        else if (RIGHT == ch)
-        {
-            stopLeftcollect = 1;
-            s++;
-        }
-    }
-
-    if (!earlyFail)
-    {
-
-        if (f == s)
-        {
-            printf("======= #current line braces match ok\n");
-        }
-        else
-        {
-            printf("======= #current line braces match FAIL#\n");
-        }
-    }
+    int c = 3;
+    int static d = 4;
+    printf("a =%d, b=%d, c=%d, d=%d\n", a, b, c, d);
 }
-
 int main(int argc, char *argv[])
 {
-
-    char str[1000];
-    while (gets(str) != NULL)
-    {
-        printf("origin: %s\n", str);
-        check(str);
-    }
-
+    x();
+    y();
     return 0;
 }
