103181aff56674a3b63feb0d81cada7688d7be4f
pythonCat <pythoncat@qq.com>
2020-12-09 22:12:49 +0800

    4.1 利用 a2 = (a1+ n/a1)/2 计算反复平方根[do while]

--- src/main.c
+++ src/main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 
-char b = 2;
-void y()
-{
-    // printf("%d\n", a); // error
-    printf("b = %d\n", b);
-}
-a = 1;
-void x()
-{
-    int c = 3;
-    int static d = 4;
-    printf("a =%d, b=%d, c=%d, d=%d\n", a, b, c, d);
-}
 int main(int argc, char *argv[])
 {
-    x();
-    y();
+
+    double n;
+    printf("input one number to compute it's sqart.\n");
+    scanf("%lf",&n);
+    double x = 1, y = (x + n / x) / 2.0;
+    do
+    {
+        x = y;
+        y = (x + n / x) / 2.0;
+        printf("n=%.2f\ty=%.5f\tx=%.5f\n", n, y, x);
+    } while (x - y > 0.001);
+
+    printf("FINAL: n=%.2f\ty=%.5f\tx=%.5f\n", n, y, x);
+
     return 0;
 }
