a150f0212c0028cdd6a4d29db4d9e9f275dbb543
pythonCat <pythoncat@qq.com>
2020-12-10 21:34:23 +0800

    4.4 自定义实现 strncpy 1

--- src/main.c
+++ src/main.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#define LENGTH 30
+
+void copy_n(char dst[], char src[], int n);
 
 int main(int argc, char *argv[])
 {
 
-    printf("check trigle! Please input three numbers:\n");
-    int a, b, c;
-    int ch;
-    while (scanf("%d %d %d", &a, &b, &c) == 3)
-    {
-        printf("what's your inputs: %d,%d,%d\n", a, b, c);
-        if (a <= 0 || b <= 0 || c <= 0)
-        {
-            printf("不是三角形\n");
-        }
-        else if (a == b && b == c)
-        {
-            printf("等边三角形\n");
-        }
-        else if (a == b || b == c || c == a)
-        {
-            printf("等腰三角形\n");
-        }
-        else if (a + b > c && a + c > b && b + c > a)
-        {
-            printf("普通三角形\n");
-        }
-        else
-        {
-            printf("不是三角形\n");
-        }
-        while (getchar() != '\n')
-            ;
-    }
+    printf("input a string as source string and counnt to copy.\n");
+    char src[LENGTH];
+    char dst[LENGTH];
+    int n;
+    scanf("%s %d", src, &n);
+    printf("what's your input:[%s]-[%d]\n", src, n);
+    copy_n(dst, src, n);
+    printf("copied string:[%s]\n", dst);
+    return EXIT_SUCCESS;
+}
 
-    return 0;
+void copy_n(char dst[], char src[], int n)
+{
+    int ss = 0, i = 0;
+    while (src[i++] != '\0')
+    {
+        ss++;
+    }
+    if (n > ss)
+    {
+        n = ss;
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        dst[i] = src[i];
+    }
+    if (n < LENGTH)
+    {
+        for (size_t i = n; i < LENGTH; i++)
+        {
+            dst[i] = '\0';
+        }
+    }
 }
