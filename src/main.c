369c465b665d123add53f767734a455628090fb2
pythonCat <pythoncat@qq.com>
2020-12-11 21:47:37 +0800

    4.5 打印输入中的相邻重复行.

--- src/main.c
+++ src/main.c
@@ -1,48 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define LENGTH 100
-
-void copy_n(char dst[], char src[], int n);
 
 int main(int argc, char *argv[])
 {
 
     printf("input a string as source string and counnt to copy.\n");
-    char src[LENGTH];
-    char dst[LENGTH];
-    int n;
-    scanf("%s %d", src, &n);
-    printf("what's your input:[%s]-[%d]\n", src, n);
-    copy_n(dst, src, n);
-    printf("copied string:[%s]\n", dst);
-    return EXIT_SUCCESS;
-}
-
-void copy_n(char dst[], char src[], int n)
-{
-    int ss = 0, rest = 0, i = 0;
-    while (src[i++] != '\0')
+    char line[LENGTH];
+    char prev[LENGTH];
+    int same = 0;
+    while (gets(line) != NULL)
     {
-        ss++;
-    }
-    if (n > LENGTH)
-    {
-        n = LENGTH;
-    }
-    if (n > ss)
-    {
-        rest = n - ss;
-        n = ss;
-    }
-    for (size_t i = 0; i < n; i++)
-    {
-        dst[i] = src[i];
-    }
-    if (rest > 0)
-    {
-        for (size_t i = n; i < rest; i++)
+        size_t size = strnlen(line, LENGTH);
+        if (strncmp(prev, line, size) == 0)
         {
-            dst[i] = '\0';
+            same++;
+            continue;
+            // puts(line);
+        }
+        else
+        {
+            if (same)
+            {
+                puts(prev);
+            }
+            same = 0;
+        }
+        strncpy(prev, line, size);
+        if (size < LENGTH)
+        {
+            // 这一句不加，显示就不正常。
+            prev[size] = '\0';
         }
     }
+    if (same)
+    {
+        puts(prev);
+    }
+
+    return EXIT_SUCCESS;
 }
